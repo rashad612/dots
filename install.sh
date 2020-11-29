50ce1f3cfbd5b5c0e24cbaae3eb4ec12d7c7cc81
Rashad Majali <majali@protonmail.com>
2020-11-28 23:46:45 -0500

    Xorg libinput conf.

--- install.sh
+++ install.sh
@@ -4,4 +4,9 @@
 echo "Installing Tmux config.."
 echo
 cp -f tmux/tmux.conf ~/.tmux.conf
+
+echo "Xorg Libinput"
+echo
+cp -f libinput/libinput.conf /usr/share/X11/xorg.conf.d/40-libinput.conf
+
 echo "done!"
